b4b720d66447b6548c12f6b7322c13b9655c723c
ChrstnDmbchr <christiandambacher@gmail.com>
2018-07-02 16:50:13 +0100

    jwt now sending out with success auth

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,26 +4,36 @@ const jwt = require('jsonwebtoken');
 const models = require('../models');
 const tokenSecret = require('../config').tokenSecret;
 
+router.get("/:userId", (req, res, next) => {
+  models.User.findById(req.params.userId)
+  .then(user => {
+    if (!user) return next({status: 404, error: 'user not found'});
+    res.status(200).send({ user });
+  })
+  .catch(err => next({status: 500, error: err}));
+});
+
 router.post("/signin", (req, res, next) => {
   models.User.findOne({username: req.body.username})
   .then(user => {
-    if (!user) return next({status: 404, error: "user not found"})
-    return Promise.all([bcrypt.compare(req.body.password, user.password), user])
+    if (!user) return next({status: 404, error: "user not found"});
+    return Promise.all([bcrypt.compare(req.body.password, user.password), user]);
   })
   .then(([result, user]) => {
     if (result) {
-      const token = jwt.sign({ id: user._id }, tokenSecret)
+      const token = jwt.sign({ id: user._id }, tokenSecret);
       res.status(200).send({
         message: "auth successful",
-        token
-      })
+        token,
+        id: user._id
+      });
     } else {
-      next({status: 401, error: "auth failed"})
+      next({status: 401, error: "auth failed"});
     }
   })
   .catch(err => {
-    next({status: 500, error: err})
-  })
+    next({status: 500, error: err});
+  });
 });
 
 module.exports = router;
