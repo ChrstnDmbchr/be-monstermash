2f0c243ad721ccd71d90d8682b8cd383b70141f9
ChrstnDmbchr <christiandambacher@gmail.com>
2018-07-06 12:42:58 +0100

    added user controller and moved code into there instead of the route

---
+++ controllers/UserController.js
@@ -0,0 +1,64 @@
+const models = require('../models');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const tokenSecret = require('../config').tokenSecret;
+const saltRounds = require('../config').saltRounds;
+
+exports.getUser = (req, res, next) => {
+  models.User.findById(req.userData.id)
+  .then(user => {
+    if (!user) return next({status: 404, error: 'user not found'});
+    res.status(200).send({
+      id: user._id,
+      username: user.username
+    });
+  })
+  .catch(err => next({status: 500, error: err}));
+}
+
+exports.signUserIn = (req, res, next) => {
+  models.User.findOne({username: req.body.username})
+  .then(user => {
+    if (!user) return next({status: 404, error: "user not found"});
+    return Promise.all([bcrypt.compare(req.body.password, user.password), user]);
+  })
+  .then(([result, user]) => {
+    if (result) {
+      const token = jwt.sign({ id: user._id }, tokenSecret, { expiresIn: "1h" });
+      res.status(200).send({
+        message: "auth successful",
+        token
+      });
+    } else {
+      next({status: 401, error: "auth failed"});
+    }
+  })
+  .catch(err => {
+    next({status: 500, error: err});
+  });
+}
+
+exports.signUserUp = (req, res, next) => {
+  models.User.findOne({ username: req.body.username })
+  .then(user => {
+    if (!user) {
+      return models.User.create({
+        username: req.body.username,
+        password: bcrypt.hashSync(req.body.password, saltRounds)
+      })
+    } else {
+      next({status: 400, error: 'username already exists'})
+    }
+  })
+  .then(newUser => {
+    const token = jwt.sign({ id: newUser._id }, tokenSecret, { expiresIn: "1h" });
+    res.status(200).send({
+      message: "user successfully created",
+      token
+    });
+  })
+  .catch(err => {
+    next({status: 500, error: err});
+  });
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,67 +1,12 @@
 const router = require('express').Router();
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const models = require('../models');
-const tokenSecret = require('../config').tokenSecret;
-const saltRounds = require('../config').saltRounds;
+const userController = require('../controllers/UserController');
+
 const checkAuth = require('../middleware/checkAuth');
 
-router.get("/", checkAuth, (req, res, next) => {
-  models.User.findById(req.userData.id)
-  .then(user => {
-    if (!user) return next({status: 404, error: 'user not found'});
-    res.status(200).send({
-      id: user._id,
-      username: user.username
-    });
-  })
-  .catch(err => next({status: 500, error: err}));
-});
+router.get("/", checkAuth, userController.getUser);
 
-router.post("/signin", (req, res, next) => {
-  models.User.findOne({username: req.body.username})
-  .then(user => {
-    if (!user) return next({status: 404, error: "user not found"});
-    return Promise.all([bcrypt.compare(req.body.password, user.password), user]);
-  })
-  .then(([result, user]) => {
-    if (result) {
-      const token = jwt.sign({ id: user._id }, tokenSecret, { expiresIn: "1h" });
-      res.status(200).send({
-        message: "auth successful",
-        token
-      });
-    } else {
-      next({status: 401, error: "auth failed"});
-    }
-  })
-  .catch(err => {
-    next({status: 500, error: err});
-  });
-});
+router.post("/signin", userController.signUserIn);
 
-router.post("/signup", (req, res, next) => {
-  models.User.findOne({ username: req.body.username })
-  .then(user => {
-    if (!user) {
-      return models.User.create({
-        username: req.body.username,
-        password: bcrypt.hashSync(req.body.password, saltRounds)
-      })
-    } else {
-      next({status: 400, error: 'username already exists'})
-    }
-  })
-  .then(newUser => {
-    const token = jwt.sign({ id: newUser._id }, tokenSecret, { expiresIn: "1h" });
-    res.status(200).send({
-      message: "user successfully created",
-      token
-    });
-  })
-  .catch(err => {
-    next({status: 500, error: err});
-  })
-})
+router.post("/signup", userController.signUserUp);
 
 module.exports = router;
