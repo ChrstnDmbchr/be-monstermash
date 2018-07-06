c7d3d50b92f9131cc251f088c05e197f1d38191f
ChrstnDmbchr <christiandambacher@gmail.com>
2018-07-06 18:17:41 +0100

    finished add new mash endpoint with tests

--- controllers/mashController.js
+++ controllers/mashController.js
@@ -1,5 +1,6 @@
 const models = require('../models');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const moment = require('moment');
 
 exports.getAll = (req, res, next) => {
   models.Mash.find({})
@@ -29,19 +30,35 @@ exports.getByID = (req, res, next) => {
     next({status: 500, error: err});
   });
 };
-// todo
+
 exports.postNewMash = (req, res, next) => {
-  res.status(200).send({
-    message: "post new route working"
+  if (!req.body.imageData) {
+    return next({status: 400, error: "imageData property not found"});
+  }
+  models.Mash.create({
+    users: [req.userData.id],
+    phase: 'body',
+    imageData: req.body.imageData,
+    votes: 0,
+    lastModified: moment().format()
   })
-}
+  .then(mash => {
+    res.status(200).send({
+      message: "new monster mash created successfully",
+      mash
+    });
+  })
+  .catch(err => {
+    next({status: 500, error: err});
+  });
+};
 
 exports.getOldest = (req, res, next) => {
   models.Mash.findOne({users: { $nin: [mongoose.Types.ObjectId(req.userData.id)] }, phase: { $ne: 'completed' }})
   .sort({lastModified: 1})
   .then(mash => {
     if (!mash) {
-      return next({status: 404, error: 'no suitable mashes found'})
+      return next({status: 404, error: 'no suitable mashes found'});
     }
     res.status(200).send({ mash });
   })
