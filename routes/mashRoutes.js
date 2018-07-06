998eda3c36517d9d823e1486989c421c6b82709f
ChrstnDmbchr <christiandambacher@gmail.com>
2018-07-06 15:34:47 +0100

    re-organised routes for readability

--- routes/mashRoutes.js
+++ routes/mashRoutes.js
@@ -7,9 +7,9 @@ router.get("/all", mashController.getAll);
 
 router.get("/getmash/:mashid", mashController.getByID)
 
-router.get("/continuemash", checkAuth, mashController.getOldest);
-
 router.post("/newmash", checkAuth, mashController.postNewMash);
+
+router.get("/continuemash", checkAuth, mashController.getOldest);
 
 router.post("/continuemash", checkAuth, mashController.postContinueMash);
 
