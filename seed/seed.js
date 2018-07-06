20329bacfae741005b520abf05131d554e7f4aab
ChrstnDmbchr <christiandambacher@gmail.com>
2018-07-06 13:55:01 +0100

    mash schema completed and seed file amended to seed both dev and test

--- seed/seed.js
+++ seed/seed.js
@@ -5,8 +5,12 @@ mongoose.Promise = Promise;
 const models = require('../models');
 
 const userData = require(`./${process.env.NODE_ENV}Data/users`);
+const mashData = require(`./${process.env.NODE_ENV}Data/monsterMashes`);
 
 function dbSeed (dbUrl) {
+  let user1
+  let user2
+
   return mongoose.connection.dropDatabase()
   .then(() => {
     console.log(`${process.env.NODE_ENV} db dropped`);
@@ -14,7 +18,23 @@ function dbSeed (dbUrl) {
   })
   .then(([userData]) => {
     console.log(`${userData.length} Users seeded`)
-    return userData
+    user1 = userData[0]._id
+    user1 = userData[1]._id
+
+    const mashes = mashData.map((mash, i) => {
+      if (i === 1) {
+        mash.users.push(user1, user2)
+        return mash
+      } else {
+        mash.users.push(user1)
+        return mash
+      };
+    });
+    
+    return Promise.all([models.Mash.insertMany(mashes), userData])
+  })
+  .then(([mashes, userData]) => {
+    console.log(`${mashes.length} Monster Mashes seeded`)
   })
   .catch(err => {
     console.log(err);
